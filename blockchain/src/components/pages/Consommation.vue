ac8a64b7f3eb386dc6cb84ff9fc9aeec48b260b5
Perrin Thibaud <thibaud.perrin@outlook.com>
2019-12-13 22:31:51 +0100

    recreate sunburst

--- blockchain/src/components/pages/Consommation.vue
+++ blockchain/src/components/pages/Consommation.vue
@@ -6,6 +6,7 @@
 
 <script>
 import * as d3 from 'd3'
+const csvTransactions = require('@/assets/data/avg-consommation-per-day.csv')
 
 export default {
   name: 'Consommation',
@@ -21,13 +22,19 @@ export default {
       color: null,
       format: d3.format(',d'),
       monthNames: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Decembre'],
-      arc: null
+      arc: null,
+      part: null,
+      g: null,
+      path: null,
+      prnt: null,
+      label: null
     }
   },
   mounted () {
     this.getRadius()
     this.getArc()
     this.initGraph()
+    this.drawGraph()
   },
   computed: {
   },
@@ -43,6 +50,100 @@ export default {
         .style('font', '10px sans-serif')
         .attr('width', _self.width)
         .attr('height', _self.height)
+    },
+    drawGraph () {
+      let self = this
+      d3.csv(csvTransactions).then(function (transactions) {
+        self.data = self.groupByTime(transactions)
+
+        let colorRange = d3.scaleLinear().domain([0, 0.5]).range(['#F4A812', '#FFED42'])
+
+        self.color = d3.scaleOrdinal(d3.quantize(colorRange, self.data.children.length + 1))
+        // color = d3.scaleOrdinal(d3.quantize(d3.interpolateRainbow, data.children.length + 1))
+        // data = transactions
+        self.part = self.partition(self.data)
+
+        self.part.each(d => { d.current = d }) // ajout des {}
+
+        self.g = self.svg.append('g')
+          .attr('transform', `translate(${self.width / 2},${self.width / 2})`)
+
+        self.path = self.g.append('g')
+          .selectAll('path')
+          .data(self.part.descendants().slice(1))
+          .join('path')
+          .attr('fill', d => {
+            while (d.depth > 1) {
+              d = d.parent
+            }
+            return self.color(d.data.name)
+          })
+          .attr('fill-opacity', d => self.arcVisible(d.current) ? (d.children ? 0.6 : 0.4) : 0)
+          .attr('d', d => self.arc(d.current))
+
+        self.path.filter(d => d.children)
+          .style('cursor', 'pointer')
+          .on('click', self.clicked)
+
+        self.path.append('title')
+          .text(d => `${d.ancestors().map(d => { return d.data.name }).reverse().join('/')}\n${self.format(d.value)}`)
+
+        self.label = self.g.append('g')
+          .attr('pointer-events', 'none')
+          .attr('text-anchor', 'middle')
+          .style('user-select', 'none')
+          .selectAll('text')
+          .data(self.part.descendants().slice(1))
+          .join('text')
+          .attr('dy', '0.35em')
+          .attr('fill-opacity', d => +self.labelVisible(d.current))
+          .attr('transform', d => self.labelTransform(d.current))
+          .text(d => { return d.data.name })
+
+        self.prnt = self.g.append('circle')
+          .datum(self.part)
+          .attr('r', self.radius)
+          .attr('fill', 'none')
+          .attr('pointer-events', 'all')
+          .on('click', self.clicked)
+
+        return self.svg.node()
+      })
+    },
+    clicked (p) {
+      let self = this
+      self.prnt.datum(p.parent || self.part)
+
+      self.part.each(d => {
+        d.target = {
+          x0: Math.max(0, Math.min(1, (d.x0 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+          x1: Math.max(0, Math.min(1, (d.x1 - p.x0) / (p.x1 - p.x0))) * 2 * Math.PI,
+          y0: Math.max(0, d.y0 - p.depth),
+          y1: Math.max(0, d.y1 - p.depth)
+        }
+      })
+
+      const t = self.g.transition().duration(750)
+
+      // Transition the data on all arcs, even the ones that aren’t visible,
+      // so that if this transition is interrupted, entering arcs will start
+      // the next transition from the desired position.
+      self.path.transition(t)
+        .tween('data', d => {
+          const i = d3.interpolate(d.current, d.target)
+          return t => { d.current = i(t) }
+        })
+        .filter(function (d) {
+          return +this.getAttribute('fill-opacity') || self.arcVisible(d.target)
+        })
+        .attr('fill-opacity', d => self.arcVisible(d.target) ? (d.children ? 0.6 : 0.4) : 0)
+        .attrTween('d', d => () => self.arc(d.current))
+
+      self.label.filter(function (d) {
+        return +this.getAttribute('fill-opacity') || self.labelVisible(d.target)
+      }).transition(t)
+        .attr('fill-opacity', d => +self.labelVisible(d.target))
+        .attrTween('transform', d => () => self.labelTransform(d.current))
     },
     arcVisible (d) {
       return d.y1 <= 2 && d.y0 >= 1 && d.x1 > d.x0
