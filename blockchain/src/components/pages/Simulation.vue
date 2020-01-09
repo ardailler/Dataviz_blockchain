c3f0e6d25d070b6b0a8307b6b926b254279ef4a2
Julian Bruyat <bruyat.ju@live.fr>
2020-01-09 10:04:14 +0100

    Contexte estimateur

--- blockchain/src/components/pages/Simulation.vue
+++ blockchain/src/components/pages/Simulation.vue
@@ -1,13 +1,29 @@
 <template>
   <div class='page-consommation'>
+    <h6 style="font-size: 1.3em; margin-left: 2em; margin-right: 2em;">
+      Les cryptomonnaies se reposent sur un système de minage : des machines
+      effectuent des calculs complexes afin de valider des transactions (le
+      <em>minage</em>). La complexité des calculs et la puissance des machines
+      augmentent avec le temps. Il est possible de réaliser un bénéfice si le
+      coût énergétique du minage est plus faible que le prix de revente de la
+      monnaie minée.
+    </h6>
+
     <div id="calcul_value"  class="row">
-      <p class="col-xs-3 col-s-3 col-m-3 col-l-3 col-3 subtitle_2 ex"><span class="cursor"></span>Taux de Hachage : <input v-model="glob_hash" type="number" placeholder="Hachage en Thash/s "></p>
-      <p class="col-xs-3 col-s-3 col-m-3 col-l-3 col-3 subtitle_2 ex"><span class="cursor"></span>Hachage personnel : <input v-model="own_hash" type="number" placeholder="Hachage en Thash/s "></p>
-      <p class="col-xs-3 col-s-3 col-m-3 col-l-3 col-3 subtitle_2 ex"><span class="cursor"></span>Taux Bitcoin :  <input v-model="taux" type="number" placeholder="prix d'un kWh"> </p>
-      <p class="col-xs-3 col-s-3 col-m-3 col-l-3 col-3 subtitle_2 ex"><span class="cursor"></span>Prix Electricité :  <input v-model="prix" type="number" placeholder="prix d'un kWh"> </p>
-      <p class="col-xs-3 col-s-3 col-m-3 col-l-3 col-3 subtitle_2 ex2"><span class="cursor"></span>Consommation machine :  <input v-model="own_cons" type="number" placeholder="prix d'un kWh"> </p>
-      <p class="col-xs-3 col-s-3 col-m-3 col-l-3 col-3 subtitle_2 ex2"><span class="cursor"></span>Gain bloc  <input v-model="gain" type="number" placeholder="gain moyen pour un bloc en BT"> </p>
-      <p class="col-xs-3 col-s-3 col-m-3 col-l-3 col-3 subtitle_2 ex2"><span class="cursor"></span>Temps bloc :  <input v-model="own_cons" type="number" placeholder="Nombre bloc découvert par heure"> </p>
+      <p class="col-xs-3 col-s-3 col-m-3 col-l-3 col-3 subtitle_2 ex"><span class="cursor"></span>Taux de Hachage : <input v-model="glob_hash" type="number" placeholder="Hachage en Thash/s ">
+      <br><small>Le taux de hachage représente la puissance de minage (la vitesse de calcul) requise pour valider des transactions</small></p>
+      <p class="col-xs-3 col-s-3 col-m-3 col-l-3 col-3 subtitle_2 ex"><span class="cursor"></span>Hachage personnel : <input v-model="own_hash" type="number" placeholder="Hachage en Thash/s ">
+      <br><small>Taux de hachage possédée par la machine utilisée</small></p>
+      <p class="col-xs-3 col-s-3 col-m-3 col-l-3 col-3 subtitle_2 ex"><span class="cursor"></span>Taux de revente :  <input v-model="taux" type="number" step="any" placeholder="prix d'un kWh">
+      <br><small>Les cryptomonnaies peuvent être échangées contre de l'argent réel avec un autre utilisateur</small> </p>
+      <p class="col-xs-3 col-s-3 col-m-3 col-l-3 col-3 subtitle_2 ex"><span class="cursor"></span>Prix Electricité :  <input v-model="prix" type="number" step="any" placeholder="prix d'un kwH">
+      <br><small>Le prix de l'électricité (en kWh) influe sur la rentabilité des équipements utilisés</small></p>
+      <p class="col-xs-3 col-s-3 col-m-3 col-l-3 col-3 subtitle_2 ex2"><span class="cursor"></span>Consommation machine :  <input v-model="own_cons" type="number" step="any" placeholder="prix d'un kWh">
+      <br><small>La quantité d'electricité consommée par la machine en kw/h</small></p>
+      <p class="col-xs-3 col-s-3 col-m-3 col-l-3 col-3 subtitle_2 ex2"><span class="cursor"></span>Gain bloc  <input v-model="gain" type="number" placeholder="gain moyen pour un bloc en BT">
+      <br><small>Nombre de cryptomonnaie moyenne gagnée lorsque l'on mine un bloc</small></p>
+      <p class="col-xs-3 col-s-3 col-m-3 col-l-3 col-3 subtitle_2 ex2"><span class="cursor"></span>Temps bloc :  <input v-model="own_cons" type="number" step="any" placeholder="Nombre bloc découvert par heure">
+      <br><small>Nombre de blocs minables par heure</small></p>
       <p class ="col-xs-3 col-s-3 col-m-3 col-l-3 col-3 subtitle_2 ex2"><button @click="greet">Estimer</button> </p>
 </div>
 <div class="row">
@@ -18,7 +34,7 @@
     <td>1 heure</td>
     <td>1 jour</td>
     <td>1 semaine</td>
-    <td>1 moi</td>
+    <td>1 mois</td>
   </tr>
   <tr>
     <td>Cout consommation d'energies</td>
@@ -28,14 +44,14 @@
     <td>{{mult(one_hour_consumption, 730)}}€</td>
   </tr>
   <tr>
-    <td>Gain de Bitcoin en euros</td>
+    <td>Gain de cryptomonnaie en euros</td>
     <td>{{one_hour_gain}}€</td>
     <td>{{mult(one_hour_gain, 24)}}€</td>
     <td>{{mult(one_hour_gain, 168)}}€</td>
     <td>{{mult(one_hour_gain, 730)}}€</td>
   </tr>
   <tr>
-    <td>Benefice  en euros</td>
+    <td>Benefice en euros</td>
     <td>{{one_hour_benefit}}€</td>
     <td>{{mult(one_hour_benefit,24)}}€</td>
     <td>{{mult(one_hour_benefit,168)}}€</td>
@@ -52,7 +68,7 @@ export default {
   name: 'Simulation',
   data () {
     return {
-      glob_hash: 107698810.68229385,
+      glob_hash: parseInt(107698810.68229385),
       own_hash: 50,
       taux: 7181.96,
       prix: 0.0467,
